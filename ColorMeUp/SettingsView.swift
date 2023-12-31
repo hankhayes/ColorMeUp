5c7661f1e34c4d74db3cd6beb2637bab255f175a
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-12-31 13:54:51 -0600

    Update SettingsView.swift

--- ColorMeUp/SettingsView.swift
+++ ColorMeUp/SettingsView.swift
@@ -8,37 +8,59 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @AppStorage ("isDarkMode") private var isDarkMode = false
+    @AppStorage ("visionType") private var visionType = "No color-blindness"
+    @State var text: String = ""
+    
     var body: some View {
-        VStack {
-            Text("Settings")
-                .font(.title)
-            List {
-                Section("Appearance") {
-                    Text("App appearance")
-                    Text("Vision type")
-                }
-                Section("Account") {
-                    HStack {
-                        Text("Email")
-                        Spacer()
-                        Button(action: {
-                            print()
-                        }, label: {
-                            Text("Change")
-                        })
+        NavigationStack {
+            VStack {
+                List {
+                    Section("Appearance") {
+                        HStack {
+                            Text("Dark mode")
+                            Spacer()
+                            Toggle("", isOn: $isDarkMode)
+                        }
+                        HStack {
+                            Text("Vision type")
+                            Spacer()
+                            Picker("", selection: $visionType, content: {
+                                Text("No color-blindness").tag(0)
+                                Text("Deuteranomaly").tag(1)
+                                Text("Protanomaly").tag(2)
+                                Text("Protanopia").tag(3)
+                                Text("Deuteranopia").tag(4)
+                                Text("Tritanomaly").tag(5)
+                                Text("Tritanopia").tag(6)
+                            })
+                        }
                     }
-                    HStack {
-                        Text("Password")
-                        Spacer()
-                        Button(action: {
-                            print()
-                        }, label: {
-                            Text("Change")
-                        })
+                    Section("Account") {
+                        HStack {
+                            Text("Email")
+                            Spacer()
+                            Button(action: {
+                                print()
+                            }, label: {
+                                Text("Change")
+                            })
+                        }
+                        HStack {
+                            Text("Password")
+                            Spacer()
+                            Button(action: {
+                                print()
+                            }, label: {
+                                Text("Change")
+                            })
+                        }
                     }
                 }
-            }
 
+            }
+            .navigationTitle("Settings")
         }
     }
 }
